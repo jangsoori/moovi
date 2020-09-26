127a7e5c4f204ac51c410a4428458644b4821052
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-26 15:14:44 +0100

    Added search field in header

---
+++ src/components/Header/Search.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSearchWrapper = styled.section`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+const StyledSearchInput = styled.input.attrs(() => ({
+  type: "text",
+  placeholder: "Search movies",
+}))`
+  background: rgba(255, 255, 255, 0.12);
+  border: none;
+  height: 60%;
+  padding: 1rem 2rem;
+  color: #fff;
+  min-width: 50rem;
+  font-family: inherit;
+  font-size: 1.6rem;
+
+  ::placeholder {
+    color: white;
+    opacity: 1;
+  }
+`;
+
+export default function Search() {
+  return (
+    <StyledSearchWrapper>
+      <StyledSearchInput />
+    </StyledSearchWrapper>
+  );
+}
