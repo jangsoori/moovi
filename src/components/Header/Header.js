350a55bcd1d22f1b32a99d7984cf9af1877d2f0c
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-26 15:15:42 +0100

    Finished header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "./Logo";
+import Search from "./Search";
 
 const StyledHeader = styled.header`
   background: ${({ theme }) => theme.colors.primary};
   height: 6.5rem;
   grid-row: 1/2;
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  align-items: center;
 `;
 
 export const Header = () => {
-  return <StyledHeader>Hi</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logo />
+      <Search />
+    </StyledHeader>
+  );
 };
 
 export default Header;
