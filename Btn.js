d09a7169ce4c75d04c7b316a6e4ca4ea0f7b035d
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-26 12:14:52 +0100

    Button for later to change theme

---
+++ Btn.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { searchMovie, setTheme } from "../redux/actions";
+
+// import styled from "styled-components";
+
+// const StyledBtn = styled.button`
+//   background-color: ${({ theme }) => theme.colors.secondary};
+// `;
+// export default function Btn() {
+//   return (
+//     <StyledBtn
+//       onClick={() => props.setTheme(props.theme === "light" ? "dark" : "light")}
+//     >
+//       Set dark
+//     </StyledBtn>
+//   );
+// }
