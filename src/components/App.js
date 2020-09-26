392e52d55bae19e3155af025e49f3c63523728d1
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-26 16:50:31 +0100

    Added sidenav and height 100% to app

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import React from "react";
 // import PropTypes from "prop-types";
 import styled from "styled-components";
 import Header from "./Header/Header";
+import Sidenav from "./Sidenav/Sidenav";
 const StyledApp = styled.section`
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: auto 1fr;
   grid-template-rows: auto 1fr;
+  height: 100%;
 `;
 
 function App() {
   return (
     <StyledApp>
       <Header />
+      <Sidenav />
     </StyledApp>
   );
 }
