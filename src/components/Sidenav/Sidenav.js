f35a6f99c299b1c74bc163e4c43522897b74d28b
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-26 16:49:52 +0100

    Added sidebar with routes

---
+++ src/components/Sidenav/Sidenav.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledSidenavWrapper = styled.nav`
+  grid-row: 2/3;
+  grid-column: 1/2;
+  padding-top: 5rem;
+  background-color: ${({ theme }) => theme.colors.sidebar};
+  height: 100%;
+  width: 24rem;
+`;
+const StyledSidenavContent = styled.section``;
+const StyledList = styled.ul`
+  display: grid;
+  grid-template-rows: repeat();
+`;
+
+const StyledListItem = styled.li``;
+const StyledLink = styled(Link)`
+  padding: 1.6rem 2rem;
+  display: block;
+  font-size: 2rem;
+  transition: all 0.25s ease;
+  :hover {
+    color: ${({ theme }) => theme.colors.primary};
+    background: rgba(255, 255, 255, 0.1);
+  }
+`;
+export default function Sidenav() {
+  return (
+    <StyledSidenavWrapper>
+      <StyledSidenavContent>
+        <StyledList>
+          <StyledListItem>
+            <StyledLink to="/new">New releases</StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink to="/top">Top rated</StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink to="/upcoming">Coming soon</StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink to="/favourites">Favourites</StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink to="/watchlater ">Watch later</StyledLink>
+          </StyledListItem>
+        </StyledList>
+      </StyledSidenavContent>
+    </StyledSidenavWrapper>
+  );
+}
