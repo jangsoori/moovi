cb48e166fe14a72d08c476c3661b77a5d6d75b28
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-27 16:47:30 +0100

    Added proptypes and fetched state to props

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -4,19 +4,29 @@ import { connect } from "react-redux";
 import Section from "../components/Section";
 import { getMovie, clearOnUnmount } from "../redux/actions";
 
-function Movie({ match, getMovie, clearOnUnmount }) {
+function Movie({ match, getMovie, clearOnUnmount, movie }) {
   useEffect(() => {
     getMovie(match.params.id);
     return () => {
       clearOnUnmount();
     };
   }, []);
-  console.log(match);
-  return <Section title="hi">dsad</Section>;
+  if (!movie) {
+    return <Section title="Loading"></Section>;
+  }
+  console.log(movie);
+  return <Section title={movie.title}>dsad</Section>;
 }
 
-Movie.propTypes = {};
+Movie.propTypes = {
+  match: PropTypes.object,
+  getMovie: PropTypes.func,
+  clearOnUnmount: PropTypes.func,
+  movie: PropTypes.object,
+};
 
-const mapStateToProps = (state, ownProps) => ({});
+const mapStateToProps = (state) => ({
+  movie: state.movie,
+});
 
 export default connect(mapStateToProps, { getMovie, clearOnUnmount })(Movie);
