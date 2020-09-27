a5760c2e22bc538e8a23886e0e4f3a1792571643
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-27 14:35:22 +0100

    Added reducers/actions for New Movies and Genres

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.API_KEY;
 
 export const searchMovie = (query) => async (dispatch) => {
   const response = await axios.get(
-    `http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1`
   );
   const data = await response.data;
 
@@ -13,6 +13,27 @@ export const searchMovie = (query) => async (dispatch) => {
   });
 };
 
+export const getNewMovies = () => async (dispatch) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.data;
+  await dispatch({
+    type: "GET_NEWEST",
+    payload: data,
+  });
+};
+
+export const getGenres = () => async (dispatch) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.data.genres;
+  await dispatch({
+    type: "GET_GENRES",
+    payload: data,
+  });
+};
 export const setTheme = (theme) => {
   return {
     type: "SET_THEME",
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,15 @@ import { combineReducers } from "redux";
 
 function moviesReducer(state = null, action) {
   switch (action.type) {
+    case "GET_NEWEST":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function searchReducer(state = null, action) {
+  switch (action.type) {
     case "SEARCH_MOVIE":
       return action.payload;
     default:
@@ -9,6 +18,14 @@ function moviesReducer(state = null, action) {
   }
 }
 
+function genresReducer(state = null, action) {
+  switch (action.type) {
+    case "GET_GENRES":
+      return action.payload;
+    default:
+      return state;
+  }
+}
 function themeReducer(state = "light", action) {
   switch (action.type) {
     case "SET_THEME":
@@ -19,6 +36,8 @@ function themeReducer(state = "light", action) {
 }
 
 export default combineReducers({
+  search: searchReducer,
   movies: moviesReducer,
+  genres: genresReducer,
   theme: themeReducer,
 });
