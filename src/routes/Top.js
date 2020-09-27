49dbb07be8d6bd9b27712e59a0a5b4223a2b681d
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-27 15:55:49 +0100

    Added logic to sections for Top and Upcoming movie

--- src/routes/Top.js
+++ src/routes/Top.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import Section from "../components/Section";
-
-export default function Top() {
-  return <Section title="Top rated"></Section>;
+import { getTopMovies } from "../redux/actions";
+import PropTypes from "prop-types";
+import MoviesGrid from "../components/MoviesGrid";
+function Top({ movies, getTopMovies }) {
+  useEffect(() => {
+    getTopMovies();
+  }, []);
+  if (!movies) {
+    return <Section title="Loading..."></Section>;
+  }
+  return (
+    <Section title="Top rated">
+      <MoviesGrid movies={movies.results} />
+    </Section>
+  );
 }
+
+Top.propTypes = {
+  getTopMovies: PropTypes.func,
+  movies: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+export default connect(mapStateToProps, { getTopMovies })(Top);
--- src/routes/Upcoming.js
+++ src/routes/Upcoming.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import Section from "../components/Section";
-
-export default function Upcoming() {
-  return <Section title="Upcoming movies"></Section>;
+import { getUpcomingMovies } from "../redux/actions";
+import PropTypes from "prop-types";
+import MoviesGrid from "../components/MoviesGrid";
+function Upcoming({ movies, getUpcomingMovies }) {
+  useEffect(() => {
+    getUpcomingMovies();
+  }, []);
+  if (!movies) {
+    return <Section title="Loading..."></Section>;
+  }
+  return (
+    <Section title="Coming soon">
+      <MoviesGrid movies={movies.results} />
+    </Section>
+  );
 }
+
+Upcoming.propTypes = {
+  getUpcomingMovies: PropTypes.func,
+  movies: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+export default connect(mapStateToProps, { getUpcomingMovies })(Upcoming);
