d976d5ccfa331c144aad64f0391bfe9c1ced17c2
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-27 14:35:52 +0100

    Added logic to New Releases section

--- src/routes/New.js
+++ src/routes/New.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import MoviesGrid from "../components/MoviesGrid";
 import Section from "../components/Section";
-export default function New() {
+import { getNewMovies } from "../redux/actions";
+import PropTypes from "prop-types";
+function New({ movies, getNewMovies }) {
+  useEffect(() => {
+    getNewMovies();
+  }, []);
+
+  if (!movies) {
+    return <Section title="Loading..."></Section>;
+  }
+
   return (
     <Section title="New releases">
-      <MoviesGrid />
+      <MoviesGrid movies={movies.results} />
     </Section>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+
+New.propTypes = {
+  getNewMovies: PropTypes.func.isRequired,
+  movies: PropTypes.object,
+};
+export default connect(mapStateToProps, { getNewMovies })(New);
