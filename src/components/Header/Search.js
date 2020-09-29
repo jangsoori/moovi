d9320a2ea2c42ca86865aa75a18f25d5124f9af9
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-29 14:08:41 +0100

    Removed redux

--- src/components/Header/Search.js
+++ src/components/Header/Search.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import { getSearchSuggestions } from "../../redux/actions";
 
 const StyledSearchWrapper = styled.form`
   height: 100%;
