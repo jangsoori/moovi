a12cc120e36d3bd4091563e90e690d9d6ea89fc0
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-29 11:08:23 +0100

    Fixed Search function (previously infinite loop)

--- src/components/Header/Search.js
+++ src/components/Header/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { getSearchSuggestions } from "../../redux/actions";
 
 const StyledSearchWrapper = styled.form`
   height: 100%;
   display: flex;
+  position: relative;
   align-items: center;
 `;
 const StyledSearchInput = styled.input.attrs(() => ({
@@ -27,9 +30,32 @@ const StyledSearchInput = styled.input.attrs(() => ({
   }
 `;
 
-export default function Search() {
+// const StyledSuggestions = styled.ul`
+//   position: absolute;
+//   left: 0;
+//   bottom: 0;
+//   ${"" /* display: flex;
+//   flex-direction: column; */}
+// `;
+// const StyledSuggestion = styled.li`
+//   ${"" /* display: block; */}
+// `;
+
+// const renderSuggestions = (list) => {
+//   return list.map((item) => {
+//     return <StyledSuggestion key={item.id}>{item.title}</StyledSuggestion>;
+//   });
+// };
+// function Suggestions({ movies }) {
+//   return (
+//     <StyledSuggestions>{movies && renderSuggestions(movies)}</StyledSuggestions>
+//   );
+// }
+
+function Search(props) {
   const [input, setInput] = React.useState("");
   const history = useHistory();
+
   return (
     <StyledSearchWrapper
       onSubmit={(e) => {
@@ -46,3 +72,10 @@ export default function Search() {
     </StyledSearchWrapper>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.suggestions,
+  };
+};
+export default connect(mapStateToProps, { getSearchSuggestions })(Search);
--- src/routes/SearchResult.js
+++ src/routes/SearchResult.js
@@ -1,29 +1,41 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import Loading from "../components/Loading";
 import MoviesGrid from "../components/MoviesGrid";
 import Section from "../components/Section";
-import { searchMovie } from "../redux/actions";
+import { clearOnUnmount, searchMovie } from "../redux/actions";
 
 function SearchResult(props) {
+  //Only fire this effect after search results (its key) change
+  /////TODO: FIX INFINITE LOOP!
+  const [loading, setLoading] = React.useState(true);
   console.log(props);
-  //Only fire this effect after search results (props.movies) change
   useEffect(() => {
     props.searchMovie(props.match.params.query);
-  }, [props.movies]);
-  if (!props.movies) {
-    return <Section title="Loading..."></Section>;
-  }
+    setLoading(false);
+    return () => {
+      setLoading(true);
+    };
+  }, [props.location.key]);
+  // if (!props.movies) {
+  //   return <Section title="Loading..."></Section>;
+  // }
 
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <Section title="Search results">
-      <MoviesGrid movies={props.movies.results} />
+      <MoviesGrid movies={props.movies} />
     </Section>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
-    movies: state.searchResults,
+    movies: state.searchResults && state.searchResults.results,
   };
 };
-export default connect(mapStateToProps, { searchMovie })(SearchResult);
+export default connect(mapStateToProps, { searchMovie, clearOnUnmount })(
+  SearchResult
+);
