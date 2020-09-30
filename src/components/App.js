77ddb5f8db9e7d30ce705225015410838b8037e4
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-30 18:29:23 +0100

    Added TMDB svg as per API rules

--- src/components/App.js
+++ src/components/App.js
@@ -14,12 +14,27 @@ const StyledApp = styled.section`
   }
 `;
 
+const StyledSVG = styled.img``;
+
+const StyledSVGLink = styled.a`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  width: 15rem;
+`;
 function App() {
   return (
     <StyledApp>
       <Header />
       <Sidenav />
       <Routes />
+      <StyledSVGLink
+        href="https://www.themoviedb.org"
+        target="_blank"
+        rel="noopener"
+      >
+        <StyledSVG src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" />
+      </StyledSVGLink>
     </StyledApp>
   );
 }
